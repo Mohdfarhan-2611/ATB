77fe52d88b65eed74646b557b02f63e677b1fd97
mhdfa <mhdfarhan2611@gmail.com>
2024-09-08 13:33:34 +0530

    Add the code

--- src/Sept/Sept_08092024/File001.java
+++ src/Sept/Sept_08092024/File001.java
@@ -1,4 +1,9 @@
 package Sept.Sept_08092024;
 
 public class File001 {
+
+    public static void main(String[] args) {
+
+         System.out.println("Hello World");
+    }
 }
--- src/Sept/Sept_09092024/File002.java
+++ src/Sept/Sept_09092024/File002.java
@@ -1,4 +1,11 @@
 package Sept.Sept_09092024;
 
 public class File002 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("New file");
+
+    }
 }
