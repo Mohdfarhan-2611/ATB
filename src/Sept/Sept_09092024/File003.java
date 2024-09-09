c49861831262faa1b53d4f88584756ab6d3233e0
mhdfa <mhdfarhan2611@gmail.com>
2024-09-09 22:36:55 +0530

    commit file003

---
+++ src/Sept/Sept_09092024/File003.java
@@ -0,0 +1,13 @@
+package Sept.Sept_09092024;
+
+public class File003 {
+
+    public static void main(String[] args) {
+
+        System.out.println("First Program");
+        System.out.println(2*2);
+        System.out.println(2-2);
+        System.out.println(2+2);
+        System.out.println(2/2);
+    }
+}
